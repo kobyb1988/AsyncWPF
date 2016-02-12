746b878a52b3ed13b616ab98d7ac2b647f89c901
kobyb1988 <kobyb@mail.ru>
2016-02-12 15:24:10 +0200

    Примеры использование асинхронныз шаблонов во View

---
+++ ViewSamples/ErrorHandlingControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Neolant.ASRM.RCPManager.Styles.CodeBehind
+{
+    public class ErrorHandlingControl : ContentControl
+    {
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof (ICommand), typeof (ErrorHandlingControl), new PropertyMetadata(default(ICommand)));
+
+        public ICommand Command
+        {
+            get { return (ICommand) GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+    }
+}
---
+++ ViewSamples/JournalMeasurementsRCPCtrl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Neolant.ASRM.RCPManager.View.Controls.BodyCtrls.JournalMeasurementsRCPCtrls
+{
+    /// <summary>
+    /// Interaction logic for JournalMeasurementsRCPCtrl.xaml
+    /// </summary>
+    public partial class JournalMeasurementsRCPCtrl : UserControl
+    {
+        public JournalMeasurementsRCPCtrl()
+        {
+            InitializeComponent();
+        }
+    }
+}
